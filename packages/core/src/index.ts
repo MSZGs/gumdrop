5b0eef5bf4473c5d1a6b7048ae56cbc9545aa9e3
Márton Visnovitz <vimtaai@inf.elte.hu>
2020-06-08 23:09:41 +0200

    Add transformers to the processor

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,10 +2,16 @@ import { Processor } from "classes/processor";
 import { getFileType } from "transformers/get-file-type";
 import { loadFile } from "transformers/load-file";
 import { splitFrontmatter } from "transformers/split-frontmatter";
+import { parseData } from "transformers/parse-data";
+import { parseContent } from "transformers/parse-content";
+import { renderTemplate } from "transformers/render-template";
 
 export const processor = new Processor();
 
 processor
   .addTransformer(getFileType)
   .addTransformer(loadFile)
-  .addTransformer(splitFrontmatter);
+  .addTransformer(splitFrontmatter)
+  .addTransformer(parseData)
+  .addTransformer(parseContent)
+  .addTransformer(renderTemplate);
